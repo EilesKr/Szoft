c8dd500409ff4cad12df2b9f95cdc70768aa1b37
EilesKr <kristof.eiles@stud.uni-corvinus.hu>
2024-02-29 10:27:05 +0100

    Számoló Gomb hozzáadása

---
+++ Gyakorlo1/SzamoloGomb.cs
@@ -0,0 +1,21 @@
+﻿namespace Gyakorlo1
+{
+    internal class SzamoloGomb : Button
+    {
+        int szam = 0;
+        public SzamoloGomb()
+        {
+            Width = 20;
+            Height = 20;
+
+            Click += SzamoloGomb_Click;
+        }
+
+        private void SzamoloGomb_Click(object? sender, EventArgs e)
+        {
+            szam += 1;
+            Text = szam.ToString();
+
+        }
+    }
+}
---
+++ SzamoloGomb/Form1.cs
@@ -0,0 +1,10 @@
+namespace SzamoloGomb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
